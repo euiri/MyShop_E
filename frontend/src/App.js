db856a93bc9df38e0f91a36c4cc61ec9b23d02bf
euiri <57123626+euiri@users.noreply.github.com>
2023-12-10 01:52:33 -0800

    Home screen and Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,27 @@
 import {Container} from 'react-bootstrap' 
+import {BrowserRouter as Router, Routes, Route} from'react-router-dom'
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 const App = () => {
   return (
     <>
-      <Header />
-      <main>
-        <Container>
-          <HomeScreen />
-        </Container>
-      </main>
-      <Footer />
+      <Router>
+        <Header />
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/" exact element={<HomeScreen />} />
+              <Route path="/product/:id" element={<ProductScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
     </>
+    
   );
 }
 
--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import products from '../products'
+import Product from '../components/Product'
+import { Col, Row } from 'react-bootstrap'
 
 const HomeScreen = () => {
   return (
-    <div>HomeScreen</div>
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map(p => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={p} />
+          </Col>
+          ))
+        }
+      </Row>
+    </>
   )
 }
 
