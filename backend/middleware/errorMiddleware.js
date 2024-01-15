981783776f31fbf15da1dde074129dca3bc97502
euiri <57123626+euiri@users.noreply.github.com>
2024-01-15 01:16:55 -0800

    (BE)errorHandler, (FE)store

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,6 @@
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500:res.statusCode
+  res.statusCode(statusCode).json({message:err.message})
+}
+
+export default errorHandler
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import errorHandler from './middleware/errorMiddleware.js'
 
 
 const app = express()
@@ -10,6 +11,6 @@ connectDB()
 
 app.use('/api/products', productRoutes)
 
-
+app.use(errorHandler)
 
 app.listen(5000, console.log('Server is running on port 5000'))
